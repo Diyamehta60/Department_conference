4f2b23adcb66f501183b913258278bc9aff29882
Anil Kumar <anilvasu@gmail.com>
2025-02-21 22:57:35 +0530

    via anil sir

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
                 <div className="flex justify-center items-center mt-4 text-white">
                     <FaCalendarAlt className="mr-2" />
                     <span className="font-body text-lg">
-                        24th & 25th April, 2025
+                        4th & 5th May, 2025
                     </span>
                 </div>
                 <h2 className="font-body text-lg text-white">
