0a8a78bb93a6c6d81bdc71cc93ab9fcee49cc06b
Anil Kumar <anilvasu@gmail.com>
2025-03-20 22:06:34 +0530

    via anil sir

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,17 @@ import { FaCalendarAlt, FaMapMarkerAlt } from 'react-icons/fa'; // Importing Rea
 
 function Hero() {
     return (
-        <div className="w-full h-screen bg-[url('/frontend.jpg')] bg-center bg-cover bg-no-repeat">
+        <div className="w-full h-screen bg-[url('/frontend.jpg')] bg-center bg-cover bg-no-repeat flex items-center justify-center">
 
+            {/* Left Logo */}
+            <div className="absolute left-[1%] top-1/2 transform -translate-y-1/2">
+                <img src="/img4.png" alt="Left Logo" className="w-50 h-28" />
+            </div>
+
+
+            {/* Center Content */}
             <div className="absolute bg-red-500/40 py-4 px-8 md:top-[45%] sm:top-[60%] left-1/2 transform -translate-x-1/2 text-center rounded-lg">
-                <h1 className='className="font-heading text-4xl  text-white font-extrabold leading-tight'>Welcome to the </h1>
+                <h1 className="font-heading text-4xl text-white font-extrabold leading-tight">Welcome to the</h1>
                 <h1 className="font-heading text-3xl text-white text-center font-light leading-tight">
                     <span className='font-bold text-4xl text-white'>1<sup>st</sup></span>
                     International Conference on Networked Computing and Data Analytics (ICNCDA-2025)
@@ -27,6 +34,12 @@ function Hero() {
                     </span>
                 </div>
             </div>
+
+            {/* Right Logo */}
+            <div className="absolute right-[15%] top-1/2 transform -translate-y-1/2">
+                <img src="/img5.png" alt="Right Logo" className="w-28 h-28" />
+            </div>
+
         </div>
     );
 }
