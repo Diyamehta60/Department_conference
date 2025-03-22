e8cff121a92e71f2929111da85053a5715d5b1d5
Anil Kumar <anilvasu@gmail.com>
2025-03-22 09:34:48 +0530

    anil sir

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ function Hero() {
             </div>
 
             {/* Right Logo */}
-            <div className="absolute right-[5%] top-1/6 transform -translate-y-1/2">
+            <div className="absolute right-[2%] top-1/6 transform -translate-y-1/2">
                 <img src="/img4.png" alt="Right Logo" className="w-50 h-28" />
             </div>
 
