ca6df20ca2d617e236e97f14fc26d1f94a756e2c
anil <anil.vashu@gmail.com>
2026-04-22 19:56:13 +0530

    via anil sir

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,6 +10,20 @@ function Homepage() {
     return (
         <>
             <Hero />
+            <div className='px-12 mt-6'>
+                <div className='rounded-md border-l-4 border-red-600 bg-red-50 px-5 py-4 text-sm text-gray-700'>
+                    <p>
+                        The{" "}
+                        <a href="#" className="text-purple-600 hover:underline">
+                            Microsoft CMT
+                        </a>{" "}
+                        service was used for managing the peer-reviewing process for this
+                        conference. This service was provided for free by Microsoft and they
+                        bore all expenses, including costs for Azure cloud services as well as
+                        for software development and support.
+                    </p>
+                </div>
+            </div>
             <div className='px-12 mt-2'>
                 <ImportantDates />
             </div>
