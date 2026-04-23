6587e68906c835b7982d1cc496e537ddfe144c96
anil <anil.vashu@gmail.com>
2026-04-23 09:08:49 +0530

    via anil sir

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
       <div className="flex-1 text-left px-4">
         <h2 className="font-bold mb-4">ICNCDA2026</h2>
         <p>
-          CMT ACKNOWLEDGMENT : The Microsoft CMT service was used for managing the peer-reviewing
+          The Microsoft CMT service was used for managing the peer-reviewing
           process for this conference. This service was provided for free by
           Microsoft and they bore all expenses, including costs for Azure cloud
           services as well as for software development and support.
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,18 +5,10 @@ import Marquee from '../components/Marquee'
 import About from '../components/About'
 import Sponsored from '../components/Sponsored'
 
-const cmtAcknowledgment =
-    "The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.";
-
 function Homepage() {
     return (
         <>
             <Hero />
-            <div className='px-12 mt-6'>
-                <div className='rounded-md border border-red-200 bg-red-50 px-5 py-4 text-sm text-gray-800'>
-                    <p>{cmtAcknowledgment}</p>
-                </div>
-            </div>
             <div className='px-12 mt-2'>
                 <ImportantDates />
             </div>
